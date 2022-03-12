88aa0dd098f6027cb7a2cd26268e4dda163f34b3
MinaMalak-cmd <engmina15@yahoo.com>
2022-03-12 19:09:38 +0200

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,9 @@
-import "./App.css";
-import { Alert } from "react-bootstrap";
+import Counter from './components/Counter';
 
 function App() {
-  let x: Number = 6;
   return (
-    <div className="">
-      {x}
-      <br />
-      {[
-        "primary",
-        "secondary",
-        "success",
-        "danger",
-        "warning",
-        "info",
-        "light",
-        "dark",
-      ].map((variant, idx) => (
-        <Alert key={idx} variant={variant}>
-          This is a {variant} alert—check it out!
-        </Alert>
-      ))}
-    </div>
+    <Counter />
+
   );
 }
 
---
+++ src/components/Counter.tsx
@@ -0,0 +1,15 @@
+const classes= require("./Counter.module.css");
+
+const Counter = () => {
+  const toggleCounterHandler = () => {};
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
