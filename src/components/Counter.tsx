407488329841f390b7f80fa8a06c2d9ab919b971
MinaMalak-cmd <engmina15@yahoo.com>
2022-03-13 20:43:55 +0200

    create class based component with error of reading props

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,18 +1,50 @@
-import { useDispatch,useSelector } from "react-redux";
+import {Component} from "react";
+import { useDispatch,useSelector,connect } from "react-redux";
+import { IncrementExpression } from "typescript";
 import store from "../store/index";
 const classes= require("./Counter.module.css");
 
-const Counter = () => {
-  const dispatch = useDispatch();
-  const value = useSelector((state:{counter:Number})=>state.counter);
-  return (
-    <main className={classes.counter}>
-      <h1>Redux Counter</h1>
-      <div className={classes.value}>-- {value} --</div>
-      <button onClick={()=>dispatch({type:"increment"})}>+</button>
-      <button className="mx-3" onClick={()=>dispatch({type:"decrement"})}>-</button>
-    </main>
-  );
-};
+// const Counter = () => {
+//   const dispatch = useDispatch();
+//   const value = useSelector((state:{counter:Number})=>state.counter);
+//   const incrementHandler =()=>{dispatch({type:"increment"})};
+//   const decrementHandler =()=>{dispatch({type:"decrement"})};
+//   return (
+//     <main className={classes.counter}>
+//       <h1>Redux Counter</h1>
+//       <div className={classes.value}>-- {value} --</div>
+//       <button onClick={incrementHandler}>+</button>
+//       <button className="mx-3" onClick={decrementHandler}>-</button>
+//     </main>
+//   );
+// };
+interface props{
+  Counter:any
+}
+class Counter extends Component{
+  incrementHandler(){this.props.increment()}
+  decrementHandler(){this.props.decrement()}
+  render(){
+    return (
+          <main className={classes.counter}>
+            <h1>Redux Counter</h1>
+            <div className={classes.value}>-- {this.props.value} --</div>
+            <button onClick={this.incrementHandler.bind(this)}>+</button>
+            <button className="mx-3" onClick={this.decrementHandler.bind(this)}>-</button>
+          </main>
+        );
 
-export default Counter;
+  }
+}
+const mapDispatchToProps = (dispatch:any) => {
+  return {
+    increment: () => dispatch({ type: 'increment' }),
+    decrement: () => dispatch({ type: 'decrement' })
+  }
+}
+const mapStateToProps=(state:{counter:number})=> {
+  return {
+    value:state.counter
+  };
+}
+export default connect(mapStateToProps,mapDispatchToProps)(Counter);
